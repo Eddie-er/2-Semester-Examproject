703a081a3760a94d0cacf4a19007a3b0d056a3b0
julianswagger <72072509+julianswagger@users.noreply.github.com>
2021-04-28 13:27:59 +0200

    was macht die kuh ?
    muuuuh

--- src/sample/BLL/Utils/Password.java
+++ src/sample/BLL/Utils/Password.java
@@ -4,6 +4,6 @@ public class Password {
     //https://howtodoinjava.com/security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
 
     public static String hashPassword(String password, byte[] salt, String SHA) {
-        
+        return null;
     }
 }
