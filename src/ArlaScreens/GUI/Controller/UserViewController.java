82d80e0a5c51f4b2281cad62fa95fa9a6fb08d1c
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-05-06 11:42:39 +0200

    DER ER ET YNDIGT LAAAAAND

--- src/ArlaScreens/GUI/Controller/UserViewController.java
+++ src/ArlaScreens/GUI/Controller/UserViewController.java
@@ -36,26 +36,9 @@ public class UserViewController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         webView.getEngine().load("https://www.arla.dk/");
         try {
-            loadExcel();
+            fillTable(excelReader.loadExcel());
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void loadExcel() throws IOException {
-        File file = new File("Data/Excel/MOCK_DATA.xlsx");
-
-        ExcelFile workbook = ExcelFile.load(file.getAbsolutePath());
-        ExcelWorksheet worksheet = workbook.getWorksheet(0);
-        String[][] sourceData = new String[100][26];
-        for (int row = 0; row < sourceData.length; row++) {
-            for (int column = 0; column < sourceData[row].length; column++) {
-                ExcelCell cell = worksheet.getCell(row, column);
-                if (cell.getValueType() != CellValueType.NULL) {
-                    sourceData[row][column] = cell.getValue().toString();
-                }
-            }
-            fillTable(sourceData);
         }
     }
 
