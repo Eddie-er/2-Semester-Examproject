fec165370a3da0d3526630fa2f635e59dcd3b89f
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-05-14 10:17:55 +0200

    Zoomie Zoomie

--- src/ArlaScreens/GUI/Controller/UserViewController.java
+++ src/ArlaScreens/GUI/Controller/UserViewController.java
@@ -6,11 +6,20 @@ import com.gembox.spreadsheet.*;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,11 +29,13 @@ import java.util.ResourceBundle;
 public class UserViewController implements Initializable {
 
     public WebView webView;
-
+    public ImageView imageView;
     public TableView tblExcel;
 
     private UserModel userModel;
     private ExcelReader excelReader;
+
+
 
     public UserViewController() {
         userModel = new UserModel();
@@ -35,33 +46,57 @@ public class UserViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         webView.getEngine().load("https://www.arla.dk/");
-        try {
-            fillTable(excelReader.loadExcel());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        webView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    if (mouseEvent.getClickCount() == 2) {
+                        Parent root = null;
+                        try {
+                            root = FXMLLoader.load(getClass().getResource("../View/ZoomedInWebView.fxml"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Stage stage = new Stage();
+                        Scene scene = new Scene(root);
+                        stage.setTitle("Website");
+                        stage.setScene(scene);
+                        stage.showAndWait();
+                    }
+                }
+            }
+        });
+                try {
+                    fillTable(excelReader.loadExcel());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-    private void fillTable(String[][] dataSource) {
-        tblExcel.getColumns().clear();
+            private void fillTable(String[][] dataSource) {
+                tblExcel.getColumns().clear();
 
-        ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
-        for (String[] row: dataSource) {
-            data.add(FXCollections.observableArrayList(row));
-        }
-        tblExcel.setItems(data);
+                ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+                for (String[] row : dataSource) {
+                    data.add(FXCollections.observableArrayList(row));
+                }
+                tblExcel.setItems(data);
 
-        for (int i = 0; i < dataSource[0].length; i++) {
-            final int currentColumn = i;
-            TableColumn<ObservableList<String>, String> column = new TableColumn<>(ExcelColumnCollection.columnIndexToName(currentColumn));
-            column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(currentColumn)));
-            column.setEditable(true);
-            column.setCellFactory(TextFieldTableCell.forTableColumn());
-            column.setOnEditCommit(
-                    (TableColumn.CellEditEvent<ObservableList<String>, String> t) -> {
-                        t.getTableView().getItems().get(t.getTablePosition().getRow()).set(t.getTablePosition().getColumn(), t.getNewValue());
-                    });
-            tblExcel.getColumns().add(column);
+                for (int i = 0; i < dataSource[0].length; i++) {
+                    final int currentColumn = i;
+                    TableColumn<ObservableList<String>, String> column = new TableColumn<>(ExcelColumnCollection.columnIndexToName(currentColumn));
+                    column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(currentColumn)));
+                    column.setEditable(true);
+                    column.setCellFactory(TextFieldTableCell.forTableColumn());
+                    column.setOnEditCommit(
+                            (TableColumn.CellEditEvent<ObservableList<String>, String> t) -> {
+                                t.getTableView().getItems().get(t.getTablePosition().getRow()).set(t.getTablePosition().getColumn(), t.getNewValue());
+                            });
+                    tblExcel.getColumns().add(column);
+                }
+            }
         }
-    }
-}
+
+
+
+
